18bfdaace60fdf6c7df81b8b5c5fa02406c29200
Raman Balyan <raman.balyan@gmail.com>
2018-12-05 10:32:10 +0530

    app setup with bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ramanbalyan:admin@localhost/zuntru'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 
 @app.route("/")
